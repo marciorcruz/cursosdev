0ea546dd7f00a4e58a3b9c0d2cd62966b1041928
Holivane Holanda <holivane@hotmail.com>
2018-06-19 18:35:14 -0300

    Inseri minha pasta

---
+++ Holivane/Pontosnotaveis/Modulos/tiposdeelementos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pontosnotaveis.Modulos
+{
+    public partial class tiposdeelementos : Form
+    {
+        public tiposdeelementos()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Holivane/Pontosnotaveis/aplicativo.cs
@@ -0,0 +1,124 @@
+﻿using Pontosnotaveis.Modulos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pontosnotaveis
+{
+    public partial class aplicativo : Form
+    {
+        private int childFormNumber = 0;
+
+        public aplicativo()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowNewForm(object sender, EventArgs e)
+        {
+            Form childForm = new Form();
+            childForm.MdiParent = this;
+            childForm.Text = "Janela " + childFormNumber++;
+            childForm.Show();
+        }
+
+        private void OpenFile(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitToolsStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void PasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void ToolBarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void StatusBarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void TileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void TileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void ArrangeIconsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.ArrangeIcons);
+        }
+
+        private void CloseAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Form childForm in MdiChildren)
+            {
+                childForm.Close();
+            }
+        }
+
+        private void tipoDeElementoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tiposdeelementos childForm = new tiposdeelementos();
+            childForm.MdiParent = this;
+            childForm.Show();
+        }
+
+        private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            linhas childForm = new linhas();
+            childForm.MdiParent = this;
+            childForm.Show();
+        }
+
+        private void estaçõesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            estacoes childForm = new estacoes();
+            childForm.MdiParent = this;
+            childForm.Show();
+        }
+
+        private void pontosNotáveisToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pontosnotaveis childForm = new pontosnotaveis();
+            childForm.MdiParent = this;
+            childForm.Show();
+        }
+    }
+}
