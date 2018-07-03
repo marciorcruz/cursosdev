94b7272b5b0274fcb8043454dad4d9e3141c3f68
Raphael Toshio Sakai <raphaeltoshio@hotmail.com>
2018-07-02 22:35:34 -0300

    Adicionada a Classe Repositório de Cliente

---
+++ Raphael/Solucao_Repositorio/Repositorio.MSDN/Repositorio.DAL/Repositorios/ClasseRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Repositorio.DAL.Repositorios.Base;
+using Repositorio.Entidades;
+
+namespace Repositorio.DAL.Repositorios
+{
+    public class ClienteRepositorio : Repositorio<Cliente>
+    {
+    }
+}
