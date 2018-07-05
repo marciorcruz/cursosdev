cca556a728c866ae64ad0483bdeff9bf888818f1
Holivane Holanda <holivane@hotmail.com>
2018-07-05 20:39:42 -0300

    Finalizado Repositorio.Entidades Criado Repositorio.Console

---
+++ Holivane/Repositorio.MSDN/Repositorio.Console/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repositorio.DAL.Repositorios;
+using Repositorio.Entidades;
+
+namespace Repositorio.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adicionar();
+        }
+
+        private static void Adicionar()
+        {
+            using (var repClientes = new ClienteRepositorio())
+            {
+                new List<Cliente>
+                {
+                    new Cliente { Nome="Microsoft", Ativo=true, CNPJ="9394.4343/0001-55", Endereco="1, Microsoft One", Telefone="11574739494"},
+                    new Cliente { Nome="Google", Ativo=true, CNPJ="1234.9494/0001-33", Endereco="12, Santa Clara, CA", Telefone="1185858483"},
+                    new Cliente { Nome="Oracle", Ativo=true, CNPJ="9876.4433/0002-44", Endereco="54, Santa Monica One", Telefone="4884848592"},
+                }.ForEach(repClientes.Adicionar);
+
+                repClientes.SalvarTodos();
+
+                System.Console.WriteLine("Clientes adicionadas com sucesso.");
+
+                System.Console.WriteLine("=======   clientes cadastrados  ========");
+                foreach (var c in repClientes.GetAll())
+                {
+                    System.Console.WriteLine("{0} - {1}", c.ClienteID, c.Nome);
+                }
+
+                System.Console.ReadKey();
+            }
+        }
+                
+    }
+}
